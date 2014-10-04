c458724d3889bf58fa6ded6b059368221738b546
h13579 <marych13579@gmail.com>
2014-10-04 11:02:34 -0700

    edit index.cgi, add welcome.js

---
+++ autotagger/welcome.js
@@ -0,0 +1,10 @@
+function filetype_chosen() {
+	var inputlist = document.getElementsByClassName('uploadfile');
+	for (var i = 0, i < inputlist.length; i++) {
+		if (inputlist[i].checked) {
+			document.getElementById(inputlist[i].value+"file").style.display="block";
+		} else {
+			document.getElementById(inputlist[i].value+"file").style.display="none";
+		}
+	}
+}
